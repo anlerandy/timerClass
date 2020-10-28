d6a5324c8e659e26882c5cd1f1e6d30ba89dcfae
anlerandy <alerandy@student.42.fr>
2020-10-28 15:31:48 +0100

    Updated: Memory tests.

--- unit_tests/tests/memory.js
+++ unit_tests/tests/memory.js
@@ -1,43 +1,47 @@
 const tap = require('tap');
-const { SECOND, wait } = require('../wait');
+const { SECOND, wait } = require('../helpers/wait');
 const Timer = require('../../index');
 
 const KBYTES = 1024;
 const MBYTES = KBYTES * KBYTES;
-// The env should be launch with next var value (50Mo)
-const MAX = 50 * MBYTES;
 
 const getOccurrencesMax = () => {
-	const timer = new Timer(SECOND);
-	const {heapTotal, rss} = process.memoryUsage();
-	const memoryMB = heapTotal / MBYTES;
-	const rssMB = rss /MBYTES;
-	console.log(rss, rssMB, heapTotal, memoryMB);
-	// x = MAX;
-	// 1 = SoT;
-	// const occurences = MAX / sizeOfTimer;
-	// console.log({occurences});
-	// return occurences;	
+	const { heapTotal, rss, external, heapUsed } = process.memoryUsage();
+	const memoryMB = parseInt(heapTotal / MBYTES);
+	const rssMB = parseInt(rss / MBYTES);
+	const extMB = parseInt(external / MBYTES);
+	const usedMB = parseInt(heapUsed / MBYTES);
+	return `${rssMB} | ${memoryMB} | ${extMB} | ${usedMB}`;
 };
 
-tap.test('Stress test', t => {
-	const max = 1000;
-	for (let i = 0; i < max; ++i) {
+async function test({ i, t }) {
+	try {
+		const timer = new Timer(SECOND / 100, { destroy: false });
 		try {
-			// console.log({i})
-			// getOccurrencesMax();
-			const timer = new Timer(SECOND / 2, {destroy: false});
-			try {
-				const promise = timer.launchTimer(wait(undefined, timer));
-				timer.abort();
-				timer.destroy();
-				await promise;
-			} catch (e) {}
-			console.log({id: timer._id});
-			getOccurrencesMax();
-		} catch (e) {
-			t.fail(e.message || e);
-		}
+			const promise = timer.launchTimer(wait(undefined, timer));
+			await promise;
+			timer.done();
+		} catch (_) { }
+		// Comment following line to try a SEGF();
+		timer.destroy();
+	} catch (e) {
+		console.log('Something is wrong, please abort.', e.message || e);
 	}
+}
+
+tap.test('Stress test', async t => {
+	const max = 300;
+	console.log('');
+	for (let i = 0; i < max; ++i) {
+		console.log('\033[1A\033[KPromise', `${i + 1}/${max} -`, getOccurrencesMax());
+		await test({ i, t });
+	}
+	console.log(
+		'\033[1A\033[K\033[1A\033[KTimers:',
+		Timer.getAll(),
+		getOccurrencesMax(),
+		'\n\n'
+	);
+	t.pass('Stress went well!');
 	t.end();
 });
